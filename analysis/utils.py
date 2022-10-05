edb7979fec452bb0cdbf8ac693e289c64acef665
deoache <daniel.ocampoh@udea.edu.co>
2022-10-05 01:47:21 +0000

    update utils

--- analysis/utils.py
+++ analysis/utils.py
@@ -9,3 +9,37 @@ def normalize(val, cut=None):
     else:
         ar = ak.to_numpy(ak.fill_none(val[cut], np.nan))
         return ar
+    
+def pad_val(
+    arr: ak.Array,
+    value: float,
+    target: int = None,
+    axis: int = 0,
+    to_numpy: bool = False,
+    clip: bool = True,
+):
+    """
+    pads awkward array up to ``target`` index along axis ``axis`` with value ``value``,
+    optionally converts to numpy array
+    """
+    if target:
+        ret = ak.fill_none(ak.pad_none(arr, target, axis=axis, clip=clip), value, axis=None)
+    else:
+        ret = ak.fill_none(arr, value, axis=None)
+    return ret.to_numpy() if to_numpy else ret
+
+def build_p4(cand: ak.Array):
+    """
+    builds a 4-vector
+    """
+    return ak.zip(
+        {
+            "pt": cand.pt,
+            "eta": cand.eta,
+            "phi": cand.phi,
+            "mass": cand.mass,
+            "charge": cand.charge,
+        },
+        with_name="PtEtaPhiMCandidate",
+        behavior=candidate.behavior,
+    )
