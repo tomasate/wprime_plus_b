953a9d58c63d53237c51922373d8ae1d7f40da40
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-10-05 20:56:52 -0500

    add simplified sample names

---
+++ analysis/data/build_simplified_samples.py
@@ -0,0 +1,37 @@
+import re
+import json
+
+
+def main():
+    """
+    builds a dictionary that maps each sample name to a simplified form
+    """
+    simplified_samples = {}
+    for year in ["2016", "2017", "2018"]:
+        fileset_path = f"/home/daniel/Desktop/udea/b_lepton_met/fileset/fileset_{year}_UL_NANO.json"
+        with open(fileset_path, "r") as f:
+            fileset = json.load(f)
+        simplified_samples[year] = {sample: "" for sample in fileset}
+
+    pattern = "(.*)_Match|(.*)_Tune"
+    for year, samples in simplified_samples.items():
+        for sample in samples:
+            match = re.match(pattern, sample)
+            if match:
+                sample_match = match[2]
+                if not sample_match:
+                    sample_match = match[1]
+            else:
+                sample_match = sample
+
+            simplified_samples[year][sample] = sample_match
+
+    output_path = (
+        "/home/daniel/Desktop/udea/b_lepton_met/analysis/data/simplified_samples.json"
+    )
+    with open(output_path, "w") as f:
+        json.dump(simplified_samples, f)
+
+
+if __name__ == "__main__":
+    main()
